807734d24a380f88b5afd78267d28d461bad57a1
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-25 12:02:48 -0500

    mock code challenge done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,95 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainDiv = document.querySelector('main');
+
+fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(trainers => {
+        trainers.forEach(function(trainer) {
+            renderTrainerCard(trainer);
+        })
+    })
+
+mainDiv.addEventListener('click', function(event){
+    if (event.target.className === "add-pokemon") {
+        addPokemon(event.target.dataset.trainerId);
+    } else if (event.target.className == "release") {
+        releasePokemon(event.target.dataset.pokemonId);
+    }
+})
+
+
+function renderTrainerCard(trainer) {
+    let trainerHTML = `
+        <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
+            <button class="add-pokemon" data-trainer-id="${trainer.id}">Add Pokemon</button>
+            <ul>
+            </ul>
+        </div>
+    `
+    mainDiv.insertAdjacentHTML('beforeend', trainerHTML);
+    trainer.pokemons.forEach(function(pokemon) {
+        renderPokemon(pokemon);
+    });
+}
+
+function renderPokemon(pokemon) {
+    let pokemonHTML = `
+        <li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+    `
+    let trainerPokemonList = document.querySelector(`[data-id="${pokemon.trainer_id}"] ul`);
+
+    trainerPokemonList.insertAdjacentHTML('beforeend', pokemonHTML);
+}
+
+
+function addPokemon(trainerId) {
+    let pokemonConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            "trainer_id": parseInt(trainerId)
+        })
+    }
+    fetch(POKEMONS_URL, pokemonConfig)
+        .then(response => {
+            return response.json();
+        })
+        .then(pokemon => {
+            if (pokemon.id) {
+                renderPokemon(pokemon);
+            } else {
+                throw Error(pokemon.error);
+            }
+        })
+        .catch(function(error) {
+            alert(error);
+        })
+}
+
+function releasePokemon(pokemonId) {
+    let releasePokemonURL = POKEMONS_URL + `/${pokemonId}`
+    let pokemonConfig = {
+        method: 'DELETE',
+    }
+    fetch(releasePokemonURL, pokemonConfig)
+        .then(response => {
+            return response.json()
+        })
+        .then(pokemon => {
+            removePokemon(pokemon.id);
+        })
+        .catch(function(error) {
+            alert(error);
+        })
+}
+
+function removePokemon(pokemonId) {
+    let pokemonReleaseButton = document.querySelector(`[data-pokemon-id="${pokemonId}"]`);
+    pokemonListItem = pokemonReleaseButton.parentNode;
+    pokemonListItem.remove();
+}
